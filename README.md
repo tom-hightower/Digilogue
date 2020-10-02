981f152b256f4251d57e533ee443281b818fe123
Tom Hightower <tom.hightower356@gmail.com>
2020-10-02 19:31:59 -0400

    Updated manufacturer uid

---
+++ README.md
@@ -0,0 +1,2 @@
+# Digilogue
+A basic MPE capable synthesier plug-in with IGraphics GUI
---
+++ projects/Digilogue-wam-controller.mk
@@ -0,0 +1,12 @@
+include ../config/Digilogue-web.mk
+
+TARGET = ../build-web/scripts/Digilogue-web.js
+
+SRC += $(WEB_SRC)
+CFLAGS += $(WEB_CFLAGS)
+CFLAGS += $(EXTRA_CFLAGS)
+LDFLAGS += $(WEB_LDFLAGS) \
+-s EXPORTED_FUNCTIONS=$(WEB_EXPORTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(SRC)
